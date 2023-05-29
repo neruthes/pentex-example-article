93b8e394dcf668f2377e0fc05ff8c9a0c86f2038
Neruthes <i@neruthes.xyz>
2023-05-29 14:43:36 +0000

    Regular update '2023-05-29T14:43:36+00:00'

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-H=pentex-example-article.H.tex pentex README.md
+# H=pentex-example-article.H.tex pentex README.md
+h=npm:pentex-example-article pentex README.md
 
 if [[ $USER == neruthes ]]; then
     minoss ./*.pdf
